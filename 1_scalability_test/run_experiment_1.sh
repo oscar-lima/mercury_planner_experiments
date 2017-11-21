8f2bb7d6f7479eb2d86a08cac4763d7edd890603
Oscar Lima <olima_84@yahoo.com>
2017-11-21 21:59:50 +0000

    delete problems 26-30 to be same with youbot + rename same plan folder to scalability test

--- 1_scalability_test/run_experiment_1.sh
+++ 1_scalability_test/run_experiment_1.sh
@@ -8,7 +8,7 @@ if [ -d "$CURRENT_DIR/runs" ]; then
     rm -rf $CURRENT_DIR/runs
 fi
 
-for i in `seq 1 $NUMBER_OF_PROBLEMS_TO_RUN`;
+for i in `seq 12 $NUMBER_OF_PROBLEMS_TO_RUN`; # HACK, change back from 12 to 1
 do
     bash $CURRENT_DIR/experiment_1.sh $i
 done
